aed0067478a95a3dede4fffff9b9d5838c7e59a1
kvnbrihadgyan <20kvnk@gmail.com>
2016-05-24 11:54:42 +0530

    Create urls.py

---
+++ androider/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from . import views
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
+app_name='androider'
+urlpatterns = [
+    url(r'^$', views.device_list, name='device_list'),
+    url(r'^?initialPoint=(?P<inicoord>[[-+]?[0-9]*\.?[0-9]+,[-+]?[0-9]*\.?[0-9]+])&?finalPoint=(?P<fincoord>[[-+]?[0-9]*\.?[0-9]+,[-+]?[0-9]*\.?[0-9]+])/coord/$', views.coord, name='coord'),
+    url(r'^/dijk/$', views.dijk, name='dijk'),
+
+]
