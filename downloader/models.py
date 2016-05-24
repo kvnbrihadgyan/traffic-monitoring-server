8e5fda69cf05184b72afe0745430550cd3182d64
kvnbrihadgyan <20kvnk@gmail.com>
2016-05-25 04:05:55 +0530

    Update models.py

--- downloader/models.py
+++ downloader/models.py
@@ -1,19 +1,4 @@
 from __future__ import unicode_literals
 
-from django.db import models
-# from uploader.models import TrafficData, DeviceData
-# Create your models here.
-from django.db import models
-
-
-class DeviceData(models.Model):
-    dev_id = models.IntegerField(primary_key=True)
-    lat = models.DecimalField(max_digits=15, decimal_places=8)
-    lon = models.DecimalField(max_digits=15, decimal_places=8)
-
-class TrafficData(models.Model):
-    device = models.ForeignKey(DeviceData, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    macadd = models.CharField(max_length=12)
-
+from main.models import TrafficData, DeviceData
 #
