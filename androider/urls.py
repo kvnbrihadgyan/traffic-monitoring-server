ab071d931299ea94f031dee2c1bb73c5c5fea8e0
kvnbrihadgyan <20kvnk@gmail.com>
2016-05-24 11:56:55 +0530

    Update urls.py
    There is an error with the regex expressions here. The second one conforms to karteek's url, but in python, throws up an error of duplication. Working on it

--- androider/urls.py
+++ androider/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from . import views
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 
 app_name='androider'
 urlpatterns = [
